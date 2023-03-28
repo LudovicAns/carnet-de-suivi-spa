2db6233356332c95c37935f0087b46fcd12512f2
LudovicAns <ludovicanselin@gmail.com>
2023-03-28 09:26:00 +0200

    add Card component

---
+++ components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import {ChildProp} from "next/dist/server/app-render";
+import styles from "./Card.module.scss";
+
+
+export default function Card({ children }: ChildProp) {
+    return (
+        <div className={styles.card}>
+            { children }
+        </div>
+    )
+}
