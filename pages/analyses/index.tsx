8ab95effc6cbe6c19fc99e8ca887e03b19e745da
LudovicAns <ludovicanselin@gmail.com>
2023-03-28 09:34:32 +0200

    front dev

--- pages/analyses/index.tsx
+++ pages/analyses/index.tsx
@@ -1,20 +1,42 @@
 import useSwr from 'swr';
 import {IAnalyse} from "@/models/analyse";
+import Card from "@/components/Card/Card";
+import styles from "../../styles/Analyses.module.scss";
+import Button from "@/components/Buttons/Button";
 
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
+const DATE_OPTIONS: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+    timeZone: 'Europe/Paris'
+};
+
 export default function Analyses() {
-    const { data, error, isLoading } = useSwr<IAnalyse[]>('/api/analyses', fetcher)
+    const { data, error, isLoading } = useSwr('/api/analyses', fetcher);
 
     return (
-        <div className="analyses">
-            { isLoading && <>Chargement des données...</>}
-            { error && <>{error}</> }
-            { data && data.map((analyse: IAnalyse, index: number) => [
-                <div key={analyse._id}>
-                    {`#${index} - ${analyse.equipement} - ${analyse.ph}`}
-                </div>
-            ]) }
+        <div className={styles.container}>
+            <div className={styles.analyses}>
+                { isLoading && <>Chargement des données...</>}
+                { error && <>{error}</> }
+                { (!error && data && data.body) && data.body.map((analyse: IAnalyse) => {
+                    const date = new Date(analyse.createdAt);
+                    const formattedDate = date.toLocaleDateString('fr-FR', DATE_OPTIONS);
+
+                    return (
+                        <Card current={} segment={} key={analyse._id}>
+                            <h1>{`Analyse du ${formattedDate}`}</h1>
+                            <Button content="Voir les résultats →" />
+                        </Card>
+                    )
+                }) }
+            </div>
         </div>
     )
 }
