cc77a358a279020e4febe4c84228766c5d684d04
LudovicAns <ludovicanselin@gmail.com>
2023-03-28 09:23:21 +0200

    update analyse model

--- models/analyse.ts
+++ models/analyse.ts
@@ -1,8 +1,15 @@
 import { Document, model, Schema } from "mongoose";
+import {number} from "prop-types";
 
 export interface IAnalyse extends Document {
     equipement: string;
     ph: number;
+    transparence: string;
+    temperatureEau: number;
+    dpd1: number;
+    dpd3: number;
+    combine: number;
+    libreActif: number;
     createdAt: Date;
     updatedAt?: Date;
 }
@@ -10,6 +17,12 @@ export interface IAnalyse extends Document {
 const AnalyseSchema: Schema = new Schema({
     equipement: { type: String, required: true },
     ph: { type: Number, required: true },
+    transparence: { type: String, required: true },
+    temperatureEau: { type: Number, required: true },
+    dpd1: { type: Number, required: true },
+    dpd3: { type: Number, required: true},
+    combine: { type: Number, required: true },
+    libreActif: { type: Number, required: true },
     createdAt: { type: Date, required: true },
     updatedAt: { type: Date, required: false }
 });
